274b0caebeb3091fcf78dca201aba2483d8de342
ksankaranara-vmw <94123215+ksankaranara-vmw@users.noreply.github.com>
2023-03-16 11:00:35 +0530

    Update Sb3DemoApplication.java

--- src/main/java/org/martinlippert/sb3/sb3demo/Sb3DemoApplication.java
+++ src/main/java/org/martinlippert/sb3/sb3demo/Sb3DemoApplication.java
@@ -2,11 +2,18 @@ package org.martinlippert.sb3.sb3demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.actuate.trace.http.HttpTraceRepository;
+import org.springframework.boot.actuate.trace.http.InMemoryHttpTraceRepository;
 
 @SpringBootApplication
 public class Sb3DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sb3DemoApplication.class, args);
+	}
+	
+	@Bean
+	public HttpTraceRepository htttpTraceRepository() {
+		return new InMemoryHttpTraceRepository();
 	}
 }
