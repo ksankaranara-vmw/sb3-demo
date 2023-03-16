0c1fbfbb5fa481b6ea9f809e206253f063773bba
ksankaranara-vmw <94123215+ksankaranara-vmw@users.noreply.github.com>
2023-03-16 10:53:29 +0530

    Update ActuatorHttpExchangesConfiguration.java

--- src/main/java/org/martinlippert/sb3/sb3demo/ActuatorHttpExchangesConfiguration.java
+++ src/main/java/org/martinlippert/sb3/sb3demo/ActuatorHttpExchangesConfiguration.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ActuatorHttpExchangesConfiguration {
     @Bean
-    public HttpExchangeRepository httpTraceRepository()
+    public HttpTraceRepository httpTraceRepository()
     {
-        return new InMemoryHttpExchangeRepository();
+        return new InMemoryHttpTraceRepository();
     }
 }
